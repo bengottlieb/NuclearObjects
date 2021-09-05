cdedbb99e990882994dac071eecc5c6a75115d76
Ben Gottlieb <ben@standalone.com>
2021-09-05 07:10:13 -0500

    Initial Commit

---
+++ NuclearTesting/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NuclearTesting
+//
+//  Created by Ben Gottlieb on 9/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NuclearTesting/NuclearTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  NuclearTestingApp.swift
+//  NuclearTesting
+//
+//  Created by Ben Gottlieb on 9/5/21.
+//
+
+import SwiftUI
+
+@main
+struct NuclearTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
