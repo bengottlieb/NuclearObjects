3de7ca49068ecb22c7af4c4dd56b118a78242e4d
Ben Gottlieb <ben@standalone.com>
2021-09-05 08:04:31 -0500

    renamed to NuclearObjects

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Nuclear",
+    name: "NuclearObjects",
 	 platforms: [
 			  .iOS(.v13),
 			  .watchOS(.v6)
@@ -12,8 +12,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "Nuclear",
-            targets: ["Nuclear"]),
+            name: "NuclearObjects",
+            targets: ["NuclearObjects"]),
     ],
 	dependencies: [
 	],
@@ -21,7 +21,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "Nuclear",
+            name: "NuclearObjects",
             dependencies: []),
     ]
 )
